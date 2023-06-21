3496e980d341be36571166ab49edc3870931225e
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-21 15:43:45 +0200

    Add app.get /send route

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const sendEmail = require('./controllers/sendEmail');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -12,8 +14,10 @@ app.use(express.json());
 
 // routes
 app.get('/', (req, res) => {
-  res.send('<h1>Email Project</h1>');
+  res.send('<h1>Email Project</h1> <a href="/send">send email</a>');
 });
+
+app.get('/send', sendEmail);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,9 +26,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    app.listen(port, () => console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
