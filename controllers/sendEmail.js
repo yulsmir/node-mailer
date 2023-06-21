0491c13e92ecd371e8be77c8b19f49dbac2eb030
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-21 15:58:23 +0200

    Setup send email

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,5 +1,25 @@
+const nodemailer = require('nodemailer');
+
 const sendEmail = async (req, res) => {
-  res.send('send email func');
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'rudy.hettinger45@ethereal.email', // test email
+      pass: 'cJB38dE4CSGwkKE5kF', // test pass
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: `Me <myemail@mail.com>`,
+    to: 'bar@example.com',
+    subject: 'Hi, test email with nodemailer',
+    html: '<h2>Sending emails with Node.js</h2>',
+  });
+
+  res.json(info);
 };
 
 module.exports = sendEmail;
